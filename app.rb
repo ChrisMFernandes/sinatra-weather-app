2eb936a52ce00c311059ef124657513dc1dd0587
ChrisMFernandes <Chrismfernandes12@gmail.com>
2018-12-02 20:22:49 -0500

    first commit

---
+++ app.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+require 'curb'
+require 'json'
+require 'pp'
+
+get '/' do
+  city = params[:city]
+  req = Curl::Easy.perform("http://api.openweathermap.org/data/2.5/weather?q=#{city}&units=imperial&appid=#{ENV['OPENWEATHER_API_KEY']}")
+  res = req.body_str
+  data = JSON.parse(res)
+  @temp = data["main"]["temp"]
+  @name = data["name"]
+  erb :index
+end
+
