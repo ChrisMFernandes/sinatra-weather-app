1c0e16c34c24da9c43b1c1ff8b5d4fda0e271de9
ChrisMFernandes <Chrismfernandes12@gmail.com>
2018-12-03 12:20:43 -0500

    added lemon css for styling

--- app.rb
+++ app.rb
@@ -7,9 +7,7 @@ get '/' do
   city = params[:city]
   req = Curl::Easy.perform("http://api.openweathermap.org/data/2.5/weather?q=#{city}&units=imperial&appid=#{ENV['OPENWEATHER_API_KEY']}")
   res = req.body_str
-  data = JSON.parse(res)
-  @temp = data["main"]["temp"]
-  @name = data["name"]
+  @data = JSON.parse(res)
   erb :index
 end
 
