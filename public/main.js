6ff76f80ac2cf8fa255498f06d424bcba5133e23
ChrisMFernandes <Chrismfernandes12@gmail.com>
2018-12-04 15:23:20 -0500

    started adding in some js for animataions

---
+++ public/main.js
@@ -0,0 +1,9 @@
+console.log('linked')
+
+let button = document.getElementById('hero-button');
+
+button.addEventListener('click', function() {
+  let text = document.getElementById('display-text');
+
+  console.log(text);
+})
